37b4b14eaaa11cd7447250b06324d3dd60cbbcf8
Arman Yessymkanov <arman.yessymkanov@nutanix.com>
2023-10-31 01:15:37 +0100

    verify route

--- src/authRoutes/index.ts
+++ src/authRoutes/index.ts
@@ -1,7 +1,11 @@
 import { Router, Request, Response, NextFunction } from "express";
 import bcrypt from "bcrypt";
 import jsonwebtoken from "jsonwebtoken";
+import { sub } from "date-fns";
+import nodemailer from "nodemailer";
 import prisma from "../prismaClient";
+import { generateRandomString, getUser } from "../utils";
+import { sendEmail } from "../utils/email";
 
 const router = Router();
 
@@ -22,8 +26,12 @@ router.post("/signin", async (req: Request, res: Response, next: NextFunction) =
       throw new Error('Not valid credentials');
     }
 
+    if (!user.isActive) {
+      throw new Error('User is not activated');
+    }
+
     const token = jsonwebtoken.sign(
-      { id: user.id, email: user.email, name: user.name, personalBoardId: user.personalBoardId, },
+      { id: user.id, email: user.email, name: user.name, personalBoardId: user.personalBoardId, isActive: user?.isActive },
       process.env.JWT_SECRET as string,
       { expiresIn: '1y' }
     );
@@ -89,12 +97,110 @@ router.post("/signup", async (req: Request, res: Response, next: NextFunction) =
       { expiresIn: '1y' },
     );
 
+    const verificationId = generateRandomString();
+
+    const ver = await prisma.verification.create({
+      data: {
+        id: verificationId,
+        token,
+      },
+    });
+
+    const link = `${process.env.CLIENT_ORIGIN}/verify/${ver.id}`;
+    sendEmail({ to: email, subject: 'Confirm your email.', name: 'verify', templateArgs: { link } });
+
     return res.status(201).json({
       data: {
-        token,
+        message: 'Sent an email with verification link',
       },
       error: null,
     });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/verify", async (req: Request, res: Response, next: NextFunction) => {
+  const { verificationId } = req.body;
+
+  try {
+    const verification = await prisma.verification.findUnique({
+      where: {
+        id: verificationId,
+      },
+    });
+
+    if (!verification) {
+      throw new Error('Incorrect link');
+    }
+
+    const tenMinutesAgo = sub(new Date(), {
+      minutes: 10,
+    });
+
+    if (verification && new Date(verification.createdAt).getTime() <= tenMinutesAgo.getTime()) {
+      throw new Error('Link has expired');
+    }
+
+    const user = getUser(verification?.token);
+
+    await prisma.user.update({
+      where: {
+        // @ts-ignore
+        id: user?.id,
+      },
+      data: {
+        isActive: true,
+      }
+    });
+
+    return res.status(200).json({
+      data: {
+        token: verification?.token,
+        error: null,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/resend", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email } = req.body;
+
+    const user = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new Error('No user with that email found.');
+    }
+
+    if (user.isActive) {
+      throw new Error('User has already been activated.');
+    }
+
+    const { id, email: userEmail, name, personalBoardId, isActive } = user;
+    const token = jsonwebtoken.sign(
+      { id, email: userEmail, name, personalBoardId, isActive, },
+      process.env.JWT_SECRET as string,
+      { expiresIn: '1y' }
+    );
+
+    const verificationId = generateRandomString();
+
+    const ver = await prisma.verification.create({
+      data: {
+        id: verificationId,
+        token,
+      },
+    });
+
+    const link = `${process.env.CLIENT_ORIGIN}/verify/${ver.id}`;
+    sendEmail({ to: email, subject: 'Confirm your email.', name: 'verify', templateArgs: { link } });
   } catch (err) {
     next(err);
   }
